a96872078aaf7c74973698aa06924b4a1582c86c
Maycol-Zx-PC <alexander51600@hotmail.com>
2023-10-20 19:25:18 -0500

    fix

--- uploadCSV.py
+++ uploadCSV.py
@@ -1,30 +1,26 @@
 import time
 from google.cloud import bigquery
 
-client = bigquery.Client()
+def uploadCsv(fileNameCsv,table_id,tableName):
+    client = bigquery.Client()
 
-job_config = bigquery.LoadJobConfig(
-    source_format = bigquery.SourceFormat.CSV,
-    skip_leading_rows = 1,
-    autodetect = True,
-)
+    job_config = bigquery.LoadJobConfig(
+        source_format = bigquery.SourceFormat.CSV,
+        skip_leading_rows = 1,
+        autodetect = True,
+    )
+    with open(fileNameCsv, "rb") as source_file:
+        job = client.load_table_from_file(source_file, table_id, job_config=job_config)
 
-#Siempre cambiar el nomrbre
-tableName = "Sala_1_Servir_resoluciones"
+    table = client.get_table(table_id)
+    print("Load {} rows and {} columns to {}".format(
+        table.num_rows, len(table.schema),table_id
+        )
+    )
+
+csvName = 'Nombre.csv'
+tableName = "NombreTablaDeEjemplo"
 table_id = f"xenon-world-399922.Maycol.{tableName}"
 
-with open(r'primera-sala-Resolution-CSV.csv',"rb") as source_file:
-    job = client.load_table_from_file(source_file,table_id,job_config=job_config)
+uploadCsv(csvName,tableName,table_id)
 
-while job.state != 'DONE':
-    time.sleep(2)
-    job.reload()
-    print(job.state)
-
-print(job.result())
-
-table = client.get_table(table_id)
-print("Load {} rows and {} columns to {}".format(
-    table.num_rows, len(table.schema),table_id
-    )
-)
